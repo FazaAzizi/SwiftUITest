1bab4f161ea40c0227a6bac9612a7909737d669c
faza azizi <faza.azizi.fa@gmail.com>
2025-04-21 15:47:37 +0700

    intial commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Faza Azizi on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
